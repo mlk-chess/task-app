b3c91a52f5b9ca15f35443f592fb7fc4931024a1
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-08-15 11:08:24 +0200

    Added footer

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,10 +1,12 @@
 <template>
     <NavBar />
     <Login />
+    <Footer />
 </template>
 
 <script setup>
 import Login from "@/components/Login.vue";
 import NavBar from "../components/NavBar.vue";
+import Footer from "../components/Footer.vue";
 
 </script>
--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -1,10 +1,12 @@
 <template>
     <NavBar />
     <Register />
+    <Footer />
 </template>
 
 <script setup>
 import Register from "@/components/Register.vue";
 import NavBar from "../components/NavBar.vue";
+import Footer from "@/components/Footer.vue"
 
 </script>
