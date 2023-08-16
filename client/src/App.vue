a42569150da1a52dc47324e88e21917490e5c302
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-08-16 09:04:42 +0200

    Fix dark mode toggle

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
+import { ref, watch } from 'vue';
 import { RouterView } from 'vue-router'
+
+const theme = ref(localStorage.getItem("theme") || "lemonade");
+
+watch(theme, (newTheme) => {
+  document.querySelector("html").setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
+});
+
+document.querySelector("html").setAttribute("data-theme", theme.value);
 </script>
 
 <template>
-  <main>
-    <RouterView />
+    <main>
+      <RouterView />
   </main>
 </template>
