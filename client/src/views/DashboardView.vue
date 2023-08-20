42dffa96b78da1732b63186700467b57b30d8dd3
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-08-20 14:07:01 +0200

    Vue - test done about drag and drop

--- client/src/views/DashboardView.vue
+++ client/src/views/DashboardView.vue
@@ -6,21 +6,45 @@
     </section> -->
 
     <section>
-        <Sortable :list="elements" item-key="id" tag="div" :options="options">
-            <template #header>
-                <header>
-                    <h1>SortableJS Vue3 Demo</h1>
-                </header>
-            </template>
+        <div class="row">
+        <div class="col-3">
+          <h3>Draggable 1</h3>
+          <draggable
+            class="list-group"
+            :list="list1"
+            group="people"
+            @change="log"
+            itemKey="name"
+          >
             <template #item="{ element, index }">
-                <div class="draggable" :key="element.id">
-                    {{ element.name }}
-                </div>
+              <div class="list-group-item">{{ element.name }} {{ index }}</div>
             </template>
-            <template #footer>
-                <footer class="draggable">A footer</footer>
+          </draggable>
+        </div>
+
+        <div class="col-3">
+          <h3>Draggable 2</h3>
+          <button @click="add"> Add + </button>
+          <draggable
+            class="list-group"
+            :list="list2"
+            group="people"
+            @change="log"
+            itemKey="name"
+          >
+            <template #item="{ element, index }">
+              <div class="list-group-item">
+                {{ index }} {{ element.name }}
+                <button @click="removeItem(index)">Supprimer</button>
+            </div>
             </template>
-        </Sortable>
+          </draggable>
+        </div>
+
+        <!-- <rawDisplayer class="col-3" :value="list1" title="List 1" />
+
+        <rawDisplayer class="col-3" :value="list2" title="List 2" /> -->
+      </div>
     </section>
 
     <Footer />
@@ -29,35 +53,52 @@
 <script setup>
 import NavBar from "../components/NavBar.vue";
 import Footer from "../components/Footer.vue";
-import Calendar from "../components/UI/Calendar.vue";
-import { Sortable } from "sortablejs-vue3";
-import { ref, watch } from 'vue';
+import { ref, watch } from "vue";
+import draggable from "vuedraggable";
 
-const elements = ref([
-    {
-        'id': 1,
-        'name': 'task1'
-    }, {
-        'id': 2,
-        'name': 'task2'
-    }, {
-        'id': 3,
-        'name': 'task3'
-    }, {
-        'id': 4,
-        'name': 'task4'
-    },
-])
+const list1 = ref([
+    { name: "John", id: 1 },
+    { name: "Joao", id: 2 },
+    { name: "Jean", id: 3 },
+    { name: "Gerard", id: 4 }
+]);
 
-const options = ref({
-    onEnd: (evt) => {
-        console.log(evt);
-    }
+const list2 = ref([
+    { name: "Juan", id: 5 },
+    { name: "Edgard", id: 6 },
+    { name: "Johnson", id: 7 }
+]);
+
+watch(list1.value, (newX) => {
+    console.table(newX)
+    console.log(newX[0].name)
 })
 
-watch(elements, (newVal, oldVal) => {
-    console.log(newVal);
-})
+const add = () => {
+    list2.value.push({ name: "Mohamed" });
+};
 
+const remove = () => {
+    list1.value.pop();
+}
+
+const removeItem = (e) => {
+    const indexToRemove = e;
+    list2.value.splice(indexToRemove, 1);
+}
+
+const replace = () => {
+    list1.value = [{ name: "Edgard" }];
+};
+
+const clone = (el) => {
+    return {
+        name: el.name + " cloned"
+    };
+};
+
+const log = (evt) => {
+    console.log(evt);
+};
 
 </script>
