718137909b36f3e8cd6856f75321b634d147d306
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-08-25 15:29:45 +0200

    Vue - List of tasks

--- client/src/views/ListsTasksView.vue
+++ client/src/views/ListsTasksView.vue
@@ -3,8 +3,9 @@
     <template v-else>
         <NavBar />
 
-        <div class="flex m-10 sm:flex-col md:flex-row">
-            <div class="card w-96 bg-base-100 shadow-xl m-10" v-for="list in lists">
+        <div class="flex m-10 sm:flex-col md:flex-row justify-center flex-wrap">
+            <div v-for="list in lists"
+                class="card w-96 bg-base-100 shadow-xl m-10 transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-emerald-500 hover:cursor-pointer duration-300">
                 <div class="card-body">
                     <h2 class="card-title">{{ list.name }}</h2>
                     <div class="card-actions justify-end">
@@ -12,7 +13,25 @@
                     </div>
                 </div>
             </div>
+            <div onclick="add.showModal()"
+                class="card w-96 bg-base-100 shadow-xl m-10 transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-transparent hover:cursor-pointer duration-300">
+                <div class="card-body flex items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <line x1="12" y1="5" x2="12" y2="19"></line>
+                        <line x1="5" y1="12" x2="19" y2="12"></line>
+                    </svg>
+                </div>
+            </div>
         </div>
+        <Modal id="add">
+            <template #header>
+                Créer une liste
+            </template>
+            <template #content>
+                <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+            </template>
+        </Modal>
 
         <Footer />
     </template>
@@ -24,6 +43,7 @@ import Footer from "@/components/Footer.vue";
 import { onMounted, ref } from "vue";
 import jsCookie from 'js-cookie'
 import Loading from "@/components/UI/Loading.vue"
+import Modal from "../components/UI/Modal.vue";
 
 const lists = ref([])
 const isLoad = ref(true);
